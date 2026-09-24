c54c85a0c68c4d311bc996924d676d060c07f4f9
Diana Pareja Caceres <dianaparejacaceres@gmail.com>
2026-09-24 16:37:21 -0500

    feat(art-inventory): implement artworks api adapter

---
+++ src/art-inventory/infrastructure/artworks-api.js
@@ -0,0 +1,14 @@
+﻿import axios from "axios";
+import { errorInterceptor } from "@/shared/infrastructure/error.interceptor.js";
+
+const artApiUrl = import.meta.env.VITE_ART_API_URL;
+const impressionismPath = import.meta.env.VITE_ART_IMPRESSIONISM_PATH;
+const modernPath = import.meta.env.VITE_ART_MODERN_PATH;
+
+const http = axios.create({
+    baseURL: artApiUrl,});
+http.interceptors.response.use(errorInterceptor.onResponse, errorInterceptor.onError);
+export class ArtworksApi {
+    getImpressionismArtworks = () => http.get(impressionismPath);
+    getModernArtworks = () => http.get(modernPath);
+}
