21f20b77009f216d9542d1494207c50fc9163c74
Diana Pareja Caceres <dianaparejacaceres@gmail.com>
2026-09-24 16:24:07 -0500

    feat(footer):add footer component with support for internationalization

---
+++ src/shared/presentation/components/footer-content.vue
@@ -0,0 +1,20 @@
+﻿<script setup>
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+</script>
+
+<template>
+  <div class="grid mt-4 p-4 justify-content-center bg-primary text-primary-contrast text-center">
+    <div class="col-12 flex flex-column align-items-center">
+      <p class="m-0 mb-2 font-semibold">{{ t('footer.copyright') }}</p>
+    </div>
+    <div class="col-12 flex flex-column align-items-center">
+      <p class="m-0 text-sm">{{ t('footer.developed-by') }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
