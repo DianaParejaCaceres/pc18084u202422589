6572bfbad8e74c9f98f42a6efc7a461586e75cb9
Diana Pareja Caceres <dianaparejacaceres@gmail.com>
2026-09-24 16:51:36 -0500

    feat(art-inventory): implement artwork list component

---
+++ src/art-inventory/presentation/components/artwork-list.vue
@@ -0,0 +1,18 @@
+﻿<script setup lang="js">
+import ArtworkItem from "./artwork-item.vue";
+const { artworks } = defineProps({
+  artworks: { type: Array, required: true }
+});
+</script>
+
+<template>
+  <div class="grid">
+    <div v-for="artwork in artworks" :key="artwork.id" class="col-12 md:col-6 lg:col-4 p-2">
+      <artwork-item :artwork="artwork" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
