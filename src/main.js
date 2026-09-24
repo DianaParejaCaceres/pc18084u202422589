aadbcbcfcda8371db7e59e3b427c390467364cfa
Diana Pareja Caceres <dianaparejacaceres@gmail.com>
2026-09-24 15:40:33 -0500

    feat (i18n): configure internationalization for spanich and english

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+import i18n from "./i18n.js";
 import { createApp } from 'vue'
 import './style.css'
 import App from './app.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(i18n)
+    .mount('#app')
